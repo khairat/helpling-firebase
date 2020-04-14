910667f5bc8193fdb2afe1811e65da201394a7b8
Ali Zahid <ali.zahid@live.com>
2020-04-14 14:39:51 +0400

    Refactoring

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,31 +6,31 @@ admin.initializeApp(functions.config().firebase)
 export const removeCommentsOnOfferRemove = functions.firestore
   .document('offers/{id}')
   .onDelete(async (doc) => {
-    const { docs } = await admin
+    const docs = await admin
       .firestore()
       .collection('comments')
       .where('itemId', '==', doc.id)
       .get()
 
-    await Promise.all(
-      docs.map(({ id }) =>
-        admin.firestore().collection('comments').doc(id).delete()
-      )
-    )
+    const batch = admin.firestore().batch()
+
+    docs.forEach(({ ref }) => batch.delete(ref))
+
+    await batch.commit()
   })
 
 export const removeCommentsOnRequestRemove = functions.firestore
   .document('requests/{id}')
   .onDelete(async (doc) => {
-    const { docs } = await admin
+    const docs = await admin
       .firestore()
       .collection('comments')
       .where('itemId', '==', doc.id)
       .get()
 
-    await Promise.all(
-      docs.map(({ id }) =>
-        admin.firestore().collection('comments').doc(id).delete()
-      )
-    )
+    const batch = admin.firestore().batch()
+
+    docs.forEach(({ ref }) => batch.delete(ref))
+
+    await batch.commit()
   })
