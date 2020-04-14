497ffc268b579c7bae25565ddbace6727a42fc94
Ali Zahid <ali.zahid@live.com>
2020-04-14 16:32:31 +0400

    Functions added

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,34 +3,93 @@ import * as functions from 'firebase-functions'
 
 admin.initializeApp(functions.config().firebase)
 
+const removeComments = async (itemId: string): Promise<void> => {
+  const docs = await admin
+    .firestore()
+    .collection('comments')
+    .where('itemId', '==', itemId)
+    .get()
+
+  const batch = admin.firestore().batch()
+
+  docs.forEach(({ ref }) => batch.delete(ref))
+
+  await batch.commit()
+}
+
+export const accept = functions.https.onCall(async ({ id, kind }, { auth }) => {
+  if (!auth) {
+    throw new functions.https.HttpsError(
+      'unauthenticated',
+      'Need to be authenticated.'
+    )
+  }
+
+  const collection = kind === 'offer' ? 'offers' : 'requests'
+
+  const item = await admin.firestore().collection(collection).doc(id).get()
+
+  const data = item.data()
+
+  if (!data) {
+    throw new functions.https.HttpsError(
+      'not-found',
+      `${kind === 'offer' ? 'Offer' : 'Request'} not found.`
+    )
+  }
+
+  const { status, userId } = data
+
+  if (status !== 'pending') {
+    throw new functions.https.HttpsError(
+      'already-exists',
+      `${kind === 'offer' ? 'Offer' : 'Request'} already accepted.`
+    )
+  }
+
+  if (auth.uid === userId) {
+    throw new functions.https.HttpsError(
+      'permission-denied',
+      `You cannot accept your own ${kind}.`
+    )
+  }
+
+  const thread = await admin
+    .firestore()
+    .collection('threads')
+    .add({
+      createdAt: new Date(),
+      itemId: item.id,
+      updatedAt: new Date(),
+      userIds: [userId, auth.uid]
+    })
+
+  await admin.firestore().collection(collection).doc(id).update({
+    helplingId: auth.uid,
+    status: 'accepted',
+    threadId: thread.id,
+    updatedAt: new Date()
+  })
+})
+
 export const removeCommentsOnOfferRemove = functions.firestore
   .document('offers/{id}')
-  .onDelete(async (doc) => {
-    const docs = await admin
-      .firestore()
-      .collection('comments')
-      .where('itemId', '==', doc.id)
-      .get()
-
-    const batch = admin.firestore().batch()
-
-    docs.forEach(({ ref }) => batch.delete(ref))
-
-    await batch.commit()
-  })
+  .onDelete((doc) => removeComments(doc.id))
 
 export const removeCommentsOnRequestRemove = functions.firestore
   .document('requests/{id}')
-  .onDelete(async (doc) => {
-    const docs = await admin
-      .firestore()
-      .collection('comments')
-      .where('itemId', '==', doc.id)
-      .get()
+  .onDelete((doc) => removeComments(doc.id))
 
-    const batch = admin.firestore().batch()
+export const updateThreadOnMessageCreate = functions.firestore
+  .document('messages/{id}')
+  .onCreate(async (doc) => {
+    const data = doc.data()
 
-    docs.forEach(({ ref }) => batch.delete(ref))
+    if (data) {
+      const { threadId } = data
 
-    await batch.commit()
+      await admin.firestore().collection('threads').doc(threadId).update({
+        updatedAt: new Date()
+      })
+    }
   })
