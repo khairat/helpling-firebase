c3ae199e24a2aa891d2629f7e9cddc110b9520eb
Ali Zahid <ali.zahid@live.com>
2020-04-27 05:20:47 +0400

    Deploy functions to multiple regions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,176 +6,181 @@ import uniq from 'lodash.uniq'
 
 admin.initializeApp(functions.config().firebase)
 
-export const fetchRequest = functions.https.onRequest((request, reply) =>
-  cors()(request, reply, async () => {
-    const {
-      query: { id, kind }
-    } = request
+export const fetchRequest = functions
+  .region('us-central1', 'europe-west2')
+  .https.onRequest((request, reply) =>
+    cors()(request, reply, async () => {
+      const {
+        query: { id, kind }
+      } = request
 
-    if (!id || !kind) {
-      reply.status(401).json({
-        error: 'Missing parameters'
+      if (!id || !kind) {
+        reply.status(401).json({
+          error: 'Missing parameters'
+        })
+
+        return
+      }
+
+      const collection = kind === 'offer' ? 'offers' : 'requests'
+
+      const itemRef = await admin
+        .firestore()
+        .collection(collection)
+        .doc(id as string)
+        .get()
+
+      const item = itemRef.data()
+
+      if (!item) {
+        reply.status(404).json({
+          error: `${kind === 'offer' ? 'Offer' : 'Request'} not found.`
+        })
+
+        return
+      }
+
+      const commentsRef = await admin
+        .firestore()
+        .collection('comments')
+        .where('itemId', '==', id)
+        .get()
+
+      const comments = commentsRef.docs.map((doc) => doc.data())
+
+      const usersRef = await admin
+        .firestore()
+        .collection('users')
+        .where(
+          'id',
+          'in',
+          uniq(
+            [
+              ...comments.map(({ userId }) => userId),
+              item.userId,
+              item.helplingId
+            ].filter(Boolean)
+          )
+        )
+        .get()
+
+      const users = keyBy(
+        usersRef.docs.map((doc) => {
+          const { id, name } = doc.data()
+
+          return {
+            id,
+            name
+          }
+        }),
+        'id'
+      )
+
+      item.createdAt = item.createdAt.toDate().toISOString()
+      item.updatedAt = item.updatedAt.toDate().toISOString()
+      item.user = users[item.userId]
+
+      delete item.userId
+
+      comments.forEach((comment) => {
+        comment.createdAt = comment.createdAt.toDate().toISOString()
+        comment.user = users[comment.userId]
+
+        delete comment.itemId
+        delete comment.itemType
+        delete comment.userId
       })
 
-      return
+      reply.json({
+        comments,
+        [kind as string]: item
+      })
+    })
+  )
+
+export const accept = functions
+  .region('us-central1', 'europe-west2')
+  .https.onCall(async ({ id, kind }, { auth }) => {
+    if (!auth) {
+      throw new functions.https.HttpsError(
+        'unauthenticated',
+        'Need to be authenticated.'
+      )
     }
 
     const collection = kind === 'offer' ? 'offers' : 'requests'
 
-    const itemRef = await admin
-      .firestore()
-      .collection(collection)
-      .doc(id as string)
-      .get()
+    const itemRef = await admin.firestore().collection(collection).doc(id).get()
 
     const item = itemRef.data()
 
     if (!item) {
-      reply.status(404).json({
-        error: `${kind === 'offer' ? 'Offer' : 'Request'} not found.`
-      })
-
-      return
+      throw new functions.https.HttpsError(
+        'not-found',
+        `${kind === 'offer' ? 'Offer' : 'Request'} not found.`
+      )
     }
 
-    const commentsRef = await admin
+    if (auth.uid === item.userId) {
+      throw new functions.https.HttpsError(
+        'permission-denied',
+        `You cannot accept your own ${kind}.`
+      )
+    }
+
+    if (['accepted', 'completed'].includes(item.status)) {
+      throw new functions.https.HttpsError('invalid-argument', 'Invalid status')
+    }
+
+    const threadRef = await admin
       .firestore()
-      .collection('comments')
-      .where('itemId', '==', id)
-      .get()
+      .collection('threads')
+      .add({
+        createdAt: new Date(),
+        itemId: id,
+        itemType: kind,
+        updatedAt: new Date(),
+        userIds: [item.userId, auth.uid]
+      })
 
-    const comments = commentsRef.docs.map((doc) => doc.data())
+    await admin.firestore().collection(collection).doc(id).update({
+      helplingId: auth.uid,
+      status: 'accepted',
+      threadId: threadRef.id,
+      updatedAt: new Date()
+    })
 
-    const usersRef = await admin
+    const userRef = await admin
       .firestore()
       .collection('users')
-      .where(
-        'id',
-        'in',
-        uniq(
-          [
-            ...comments.map(({ userId }) => userId),
-            item.userId,
-            item.helplingId
-          ].filter(Boolean)
-        )
-      )
+      .doc(auth.uid)
       .get()
 
-    const users = keyBy(
-      usersRef.docs.map((doc) => {
-        const { id, name } = doc.data()
+    const user = userRef.data()
 
-        return {
-          id,
-          name
+    if (user) {
+      await admin.messaging().sendToTopic(`user_${item.userId}`, {
+        data: {
+          deeplink: `helpling://${collection}/${id}`
+        },
+        notification: {
+          body:
+            kind === 'offer'
+              ? `${user.name} accepted your offer to help.`
+              : `${user.name} has accepted your request for help.`,
+          title: `${kind === 'offer' ? 'Offer' : 'Request'} accepted`
         }
-      }),
-      'id'
-    )
+      })
+    }
 
-    item.createdAt = item.createdAt.toDate().toISOString()
-    item.updatedAt = item.updatedAt.toDate().toISOString()
-    item.user = users[item.userId]
-
-    delete item.userId
-
-    comments.forEach((comment) => {
-      comment.createdAt = comment.createdAt.toDate().toISOString()
-      comment.user = users[comment.userId]
-
-      delete comment.itemId
-      delete comment.itemType
-      delete comment.userId
-    })
-
-    reply.json({
-      comments,
-      [kind as string]: item
-    })
-  })
-)
-
-export const accept = functions.https.onCall(async ({ id, kind }, { auth }) => {
-  if (!auth) {
-    throw new functions.https.HttpsError(
-      'unauthenticated',
-      'Need to be authenticated.'
-    )
-  }
-
-  const collection = kind === 'offer' ? 'offers' : 'requests'
-
-  const itemRef = await admin.firestore().collection(collection).doc(id).get()
-
-  const item = itemRef.data()
-
-  if (!item) {
-    throw new functions.https.HttpsError(
-      'not-found',
-      `${kind === 'offer' ? 'Offer' : 'Request'} not found.`
-    )
-  }
-
-  if (auth.uid === item.userId) {
-    throw new functions.https.HttpsError(
-      'permission-denied',
-      `You cannot accept your own ${kind}.`
-    )
-  }
-
-  if (['accepted', 'completed'].includes(item.status)) {
-    throw new functions.https.HttpsError('invalid-argument', 'Invalid status')
-  }
-
-  const threadRef = await admin
-    .firestore()
-    .collection('threads')
-    .add({
-      createdAt: new Date(),
-      itemId: id,
-      itemType: kind,
-      updatedAt: new Date(),
-      userIds: [item.userId, auth.uid]
-    })
-
-  await admin.firestore().collection(collection).doc(id).update({
-    helplingId: auth.uid,
-    status: 'accepted',
-    threadId: threadRef.id,
-    updatedAt: new Date()
+    return {
+      threadId: threadRef.id
+    }
   })
 
-  const userRef = await admin
-    .firestore()
-    .collection('users')
-    .doc(auth.uid)
-    .get()
-
-  const user = userRef.data()
-
-  if (user) {
-    await admin.messaging().sendToTopic(`user_${item.userId}`, {
-      data: {
-        deeplink: `helpling://${collection}/${id}`
-      },
-      notification: {
-        body:
-          kind === 'offer'
-            ? `${user.name} accepted your offer to help.`
-            : `${user.name} has accepted your request for help.`,
-        title: `${kind === 'offer' ? 'Offer' : 'Request'} accepted`
-      }
-    })
-  }
-
-  return {
-    threadId: threadRef.id
-  }
-})
-
-export const complete = functions.https.onCall(
-  async ({ id, kind }, { auth }) => {
+export const complete = functions
+  .region('us-central1', 'europe-west2')
+  .https.onCall(async ({ id, kind }, { auth }) => {
     if (!auth) {
       throw new functions.https.HttpsError(
         'unauthenticated',
@@ -237,8 +242,7 @@ export const complete = functions.https.onCall(
         }
       })
     }
-  }
-)
+  })
 
 const cleanUpAfterItem = async (itemId: string): Promise<void> => {
   const comments = await admin
